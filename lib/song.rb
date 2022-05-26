a33283845c83c1f2d8d5a8f76282bbb919271af8
Matthew <mattj20@gmail.com>
2022-05-25 20:37:01 -0700

    commit tree b89c4ecb9a950ece95c3754c80764fbd4fae11d0

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+require 'pry'
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+    
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        new_hash = {}
+        @@genres.each do |genre|
+            if new_hash.keys.include?(genre)
+                new_hash[genre] += 1
+            else
+                new_hash[genre] = 1
+            end
+        end
+        new_hash
+    end
+
+    def self.artist_count
+        new_hash = {}
+        @@artists.each do |artist|
+            if new_hash.keys.include?(artist)
+                new_hash[artist] += 1
+            else
+                new_hash[artist] = 1
+            end
+        end
+        new_hash
+    end
+end
